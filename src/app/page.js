e2abc0ac81104b864f3677a0506ed6878751e3c6
Conrad Kuzmick <33187769+ckuzmick@users.noreply.github.com>
2023-11-15 11:04:42 -0500

    does this work??!!!

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,6 @@ import { useEffect, useRef } from "react";
 function MyThree() {
   const refContainer = useRef(null);
   useEffect(() => {
-    // === THREE.JS CODE START ===
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     var renderer = new THREE.WebGLRenderer();
@@ -15,7 +14,7 @@ function MyThree() {
     // use ref as a mount point of the Three.js scene instead of the document.body
     refContainer.current && refContainer.current.appendChild( renderer.domElement );
     var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var material = new THREE.MeshNormalMaterial({ wireframe: true });
     var cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
     camera.position.z = 5;
