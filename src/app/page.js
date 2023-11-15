467f7574c43fb079a3cc79c994f7df4f96c648f4
Conrad Kuzmick <33187769+ckuzmick@users.noreply.github.com>
2023-11-15 10:28:02 -0500

    test

--- src/app/page.js
+++ src/app/page.js
@@ -8,22 +8,22 @@ import * as THREE from 'three';
 import '../../public/packers.jpg';
 import { useRef } from 'react';
 
-function MyRotatingBox() {
-  // const myMesh = useRef();
+// function MyRotatingBox() {
+//   const myMesh = useRef();
 
-  // useFrame(({ clock }) => {
-  //   const a = clock.getElapsedTime();
-  //   myMesh.current.rotation.x = (a*a) / 5;
-  //   myMesh.current.rotation.y = a*2;
-  // });
+//   useFrame(({ clock }) => {
+//     const a = clock.getElapsedTime();
+//     myMesh.current.rotation.x = (a*a) / 5;
+//     myMesh.current.rotation.y = a*2;
+//   });
 
-  return (
-    <mesh ref={myMesh}>
-      <boxGeometry />
-      <meshNormalMaterial wireframe={true} />
-    </mesh>
-  );
-}
+//   return (
+//     <mesh ref={myMesh}>
+//       <boxGeometry />
+//       <meshNormalMaterial wireframe={true} />
+//     </mesh>
+//   );
+// }
 
 export default function Home() {
   return (
@@ -32,11 +32,11 @@ export default function Home() {
         <Canvas style={{ height: '100vh' }}>
           <ambientLight intensity={100} />
           <directionalLight color="white" intensity={10} position={[0, 1, 10]} />
-          {/* <mesh rotation={[0, 0, 0]} position={[0, 0, 0]}>
+          <mesh rotation={[0, 0, 0]} position={[0, 0, 0]}>
             <boxGeometry args={[10, 2, 10]}/>
             <meshNormalMaterial wireframe={true}/>
-          </mesh> */}
-          <MyRotatingBox/>
+          </mesh>
+          {/* <MyRotatingBox/> */}
         </Canvas>
       </div>
     </main>
