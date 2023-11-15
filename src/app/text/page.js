cca2fa85922ccb21b21c451b2b5fd8dbfe78437f
Conrad Kuzmick <33187769+ckuzmick@users.noreply.github.com>
2023-11-15 12:26:51 -0500

    Client Side Exception Error Fix (1)

--- src/app/text/page.js
+++ src/app/text/page.js
@@ -1,27 +1,34 @@
 'use client'
 
 import * as THREE from 'three';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef } from "react";
 
-function NewText() {
-    const container = useRef(null)
-    useEffect(() => {
-        var scene = new THREE.Scene();
-        var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        var renderer = new THREE.WebGLRenderer();
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        // document.body.appendChild( renderer.domElement );
-        // use ref as a mount point of the Three.js scene instead of the document.body
-        refContainer.current && refContainer.current.appendChild( renderer.domElement );
-        var geometry = new THREE.BoxGeometry(3, 3, 3);
-        var material = new THREE.MeshNormalMaterial({ wireframe: true });
-        var cube = new THREE.Mesh(geometry, material);
-        scene.add(cube);
-        camera.position.z = 5;
-    });
-    return (
-        <div ref={container}/>
-    );
+function MyThree() {
+  const refContainer = useRef(null);
+  useEffect(() => {
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    // document.body.appendChild( renderer.domElement );
+    // use ref as a mount point of the Three.js scene instead of the document.body
+    refContainer.current && refContainer.current.appendChild( renderer.domElement );
+    var geometry = new THREE.BoxGeometry(3, 3, 3);
+    var material = new THREE.MeshNormalMaterial({ wireframe: true });
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    camera.position.z = 5;
+    var animate = function () {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render(scene, camera);
+    };
+    animate();
+  }, []);
+  return (
+    <div ref={refContainer}/>
+  );
 };
 
-export default NewText;
+export default MyThree;
