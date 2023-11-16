cd4b544de0956f33165e9f4a888711d1c8e6f8f4
ckuzmick <conrad.kuzmick@icloud.com>
2023-11-15 20:21:58 -0500

    changes

--- src/app/text/page.js
+++ src/app/text/page.js
@@ -3,6 +3,8 @@
 import * as THREE from 'three';
 import { useEffect, useRef } from "react";
 import '@/../public/packers.jpg';
+import { CSS2DObject, CSS2DRenderer } from 'three/examples/jsm/renderers/CSS2DRenderer';
+import '../globals.css';
 
 function MyThree() {
   const refContainer = useRef(null);
@@ -10,28 +12,46 @@ function MyThree() {
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     var renderer = new THREE.WebGLRenderer();
-    const bg = new THREE.TextureLoader().load('@/../public/packers.jpg');
-    scene.scene.background = bg;
-    renderer.setSize(window.innerWidth, window.innerHeight);
+      renderer.setSize(window.innerWidth, window.innerHeight);
+
     // document.body.appendChild( renderer.domElement );
     // use ref as a mount point of the Three.js scene instead of the document.body
+
     refContainer.current && refContainer.current.appendChild( renderer.domElement );
-    var geometry = new THREE.BoxGeometry(3, 3, 3);
+
+    var labelRenderer = new CSS2DRenderer();
+      labelRenderer.setSize(window.innerWidth, window.innerHeight);
+      labelRenderer.domElement.style.position = 'absolute';
+      labelRenderer.domElement.style.top = '0px';
+      labelRenderer.domElement.style.color = 'white';
+
+      refContainer.current && refContainer.current.appendChild( labelRenderer.domElement );
+
+    var h1 = document.createElement('h1');
+      h1.textContent = 'Who is really titletown?';
+      var testP = new CSS2DObject(h1)  
+      scene.add(testP);
+      testP.position.set(0,2,0);
+
+    var geometry = new THREE.BoxGeometry(2, 1, .5);
     var material = new THREE.MeshNormalMaterial({ wireframe: true });
-    var mapped = new THREE.MeshStandardMaterial({ map: bg });
-    var cube = new THREE.Mesh(geometry, mapped);
-    scene.add(cube);
+    var cube = new THREE.Mesh(geometry, material);
+      scene.add(cube);
     camera.position.z = 5;
     var animate = function () {
       requestAnimationFrame(animate);
       cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
+      // cube.rotation.y += 0.01;
+      
       renderer.render(scene, camera);
+      labelRenderer.render(scene, camera);
     };
     animate();
   }, []);
   return (
+    <main>
     <div ref={refContainer}/>
+    </main>
   );
 };
 
